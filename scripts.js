d8e9561c47fb39e55a0975e5199a14d4f4019bbe
felintob <bfelinto@gmail.com>
2021-09-20 14:09:40 -0300

    Implementação das funções do jogo

---
+++ scripts.js
@@ -0,0 +1,61 @@
+const yourShip = document.querySelector('.player-shooter');
+const playArea = document.querySelector('#main-play-game');
+
+function flyAhip(event) {
+    if(event.key === 'ArrawUp') {
+        event.preventDefault();
+        moveUp();
+    } else if (event.key === 'ArrowDown') {
+        event.preventDefault();
+        moveDown();
+    } else if (event.key === " " ) {
+        event.preventDefault();
+        fireLaser();
+    }
+
+}
+
+function moveUp() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "0px") {
+        return
+    } else {
+        let position = parseInt(topPosition);
+        position -= 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+function moveDown() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if (ropPosition === "510px") {
+        return
+    } else {
+        let position = parseInt(topPosition);
+        position += 50;
+        yourHip.style.top = `${position}px`;
+    }
+}
+
+function fireLaser() {
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser();
+}
+
+function createLaserElement() {
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+    newLaser.src = 'shoot.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left = `${xPosition}px`;
+    newLaser.style.top = `${yPosition - 10}px`;
+    return newLaser;
+}
+
+function moveLaser() {
+    
+}
+
+window.addEventListener('Keydown', flyAhip);
