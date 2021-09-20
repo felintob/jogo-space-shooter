bcc8c0571968abe9805b65ed9d4f4a7c3896eda2
felintob <bfelinto@gmail.com>
2021-09-20 14:22:56 -0300

    funções para atirar

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const yourShip = document.querySelector('.player-shooter');
-const playArea = document.querySelector('#main-play-game');
+const playArea = document.querySelector('#main-play-area');
 
 function flyAhip(event) {
     if(event.key === 'ArrawUp') {
@@ -33,29 +33,37 @@ function moveDown() {
     } else {
         let position = parseInt(topPosition);
         position += 50;
-        yourHip.style.top = `${position}px`;
+        yourShip.style.top = `${position}px`;
     }
 }
 
 function fireLaser() {
     let laser = createLaserElement();
     playArea.appendChild(laser);
-    moveLaser();
+    moveLaser(laser);
 }
 
 function createLaserElement() {
     let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
     let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
     let newLaser = document.createElement('img');
-    newLaser.src = 'shoot.png';
+    newLaser.src = '/shoot.png';
     newLaser.classList.add('laser');
     newLaser.style.left = `${xPosition}px`;
     newLaser.style.top = `${yPosition - 10}px`;
     return newLaser;
 }
 
-function moveLaser() {
-    
+function moveLaser(laser) {
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left + 'px');
+
+        if (xPosition === 340) {
+            laser.remove();
+        } else {
+            laser.style.left = `${xPosition + 8}px`;
+        }
+    }, 10);
 }
 
 window.addEventListener('Keydown', flyAhip);
